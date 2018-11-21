98db767193a1a21eab8044fbaec0cadd0f9f9adf
Oleg Zilberman <olegzilberman.zilberman@gmail.com>
2018-11-21 10:56:52 -0800

    rename

--- BlueRock/app/src/main/java/bluerock/com/bluerock/data-model/FragmentCreationDescriptor.kt
+++
@@ -1,5 +0,0 @@
-package bluerock.com.bluerock.`data-model`
-
-import android.support.v4.app.Fragment
-
-data class FragmentCreationDescriptor(val fragment:Fragment, val resourceID:Int, val optionalURL:String)
--- BlueRock/app/src/main/java/bluerock/com/bluerock/item-detail/ItemDetail.kt
+++
@@ -1,5 +0,0 @@
-package bluerock.com.bluerock.`item-detail`
-
-import android.widget.ImageView
-
-data class ItemDetail(val imageName:String, val imageUrl:String, val like_count:Int, val authorName:String)
